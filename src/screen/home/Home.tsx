9c15fde40e0114a5a266b8c16a2c08ceb36e7c56
simon <simon@sayto.top>
2022-07-22 16:41:09 +0800

    feat: 添加antd

--- src/screen/home/Home.tsx
+++ src/screen/home/Home.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Text} from 'react-native';
 import {useUserDispatcher} from '../../helper/hooks';
+import {Icon, Button} from '@ant-design/react-native';
 
 const Home: React.FC = () => {
   const [userDispatcher] = useUserDispatcher();
   return (
     <View>
       <Text>Home</Text>
+      <Icon name="home" />
       <Button
-        title="退出登录"
+        type="primary"
         onPress={() => {
           userDispatcher.logout();
-        }}
-      />
+        }}>
+        退出登录
+      </Button>
     </View>
   );
 };
