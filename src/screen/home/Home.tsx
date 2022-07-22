9fb25b26c639c64c4edc5caecd533194de0b7530
simon <simon@sayto.top>
2022-07-22 15:51:20 +0800

    style: 移除遗留代码

--- src/screen/home/Home.tsx
+++ src/screen/home/Home.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import {View, Text, Button} from 'react-native';
 import {useUserDispatcher} from '../../helper/hooks';
-import {Button as AButton} from '@rneui/themed';
 
 const Home: React.FC = () => {
   const [userDispatcher] = useUserDispatcher();
   return (
     <View>
       <Text>Home</Text>
-      <AButton title="测试" />
       <Button
         title="退出登录"
         onPress={() => {
