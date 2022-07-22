96706854a8dc21bae792230684fc8c54f2c4dc6c
simon <simon@sayto.top>
2022-07-22 15:50:31 +0800

    feat: 移除ant

--- src/redux/user/dispatcher.ts
+++ src/redux/user/dispatcher.ts
@@ -5,7 +5,6 @@ import {CertificateParam, LoginParam} from '../../models';
 export interface UserDispatcher {
   login(param: LoginParam): void;
   logout(): void;
-  getUserInfo(): void;
   resetLoginState(): void;
   certificate(param: CertificateParam): void;
   getWallet(): void;
@@ -16,7 +15,6 @@ export const getUserDispatcher = (dispatch: Dispatch): UserDispatcher => ({
   login: (param: LoginParam) => dispatch(Actions.login(param)),
   logout: () => dispatch(Actions.logout()),
   resetLoginState: () => dispatch(Actions.resetLoginState()),
-  getUserInfo: () => dispatch(Actions.getUserInfo()),
   certificate: (param: CertificateParam) =>
     dispatch(Actions.certificate(param)),
   getWallet: () => dispatch(Actions.getWalletInfo()),
