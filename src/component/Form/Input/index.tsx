30a1eaedbfd840406bc8ee4bd10856c6314b1ad0
simon <simon@sayto.top>
2022-07-28 15:32:32 +0800

    feat: 封装Input组件，支持number类型

--- src/component/Form/Input/index.tsx
+++ src/component/Form/Input/index.tsx
@@ -4,11 +4,25 @@ import {InputItemProps} from '@ant-design/react-native/lib/input-item';
 import {globalStyleVariables} from '../../../constants/styles';
 
 const Input: React.FC<InputItemProps> = props => {
+  const {value, type, onChange, styles, ...restProps} = props;
+  let wrappedValue = value;
+  let wrappedOnChange = onChange;
+  if (type === 'number') {
+    wrappedValue = String(value);
+    wrappedOnChange = (value: string) => {
+      const number = Number(value) as unknown as string;
+      onChange(number);
+      onChange(number);
+    };
+  }
   return (
     <InputItem
       placeholderTextColor={globalStyleVariables.TEXT_COLOR_TERTIARY}
-      {...props}
+      value={wrappedValue}
+      {...restProps}
+      onChange={wrappedOnChange}
       styles={{
+        ...styles,
         container: {height: '100%'},
       }}
     />
@@ -21,5 +35,6 @@ Input.defaultProps = {
   type: 'text',
   placeholder: '请输入',
   labelNumber: 2,
+  styles: {},
 };
 export default Input;
