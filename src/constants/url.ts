58a1d43b28b76989b22c3f1b470e0811fe35932f
hongguoqiang <2310087518@22.comm>
2023-01-03 18:10:41 +0800

    fix:修改url

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,6 +1,6 @@
 export function getBaseURL(): string {
-  if (__DEV__) {
-    return 'https://api-beta-b-gateway.faya.life/crm-service'; // 测试
-  }
-  return 'https://api-b-gateway.faya.life/crm-service';
+  // if (__DEV__) {
+  return 'https://api-beta-b-gateway.faya.life/crm-service'; // 测试
+  // }
+  // return 'https://api-b-gateway.faya.life/crm-service';
 }
